dac0bb6bfd3513850ac64717b444cd1cf97e0295
Almas Hosain Rifat <almashossainrifat@gmail.com>
2025-08-27 05:15:07 +0600

    added js in other sections

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ for(let i = 0; i < heartBtns.length; i++ ) {
     })
 
 }
+
+// Copy Icon Click Counter 
+
+
+const copyCounter = document.querySelector('#copy button');
+
+const copyBtns = document.querySelectorAll('.copy_btn');
+
+let copyCount = 0;
+
+for (let i = 0; i < copyBtns.length; i++ ) {
+    copyBtns[i].addEventListener('click',function(){
+        const copies  = copyBtns[i].closest('.copy');
+        const call = copies.querySelector('.emergency').textContent
+    
+
+copyCount++
+copyCounter.textContent = copyCount })
+    }
