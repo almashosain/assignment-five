4054ca39cbc3391ac5cd437a83f76e8a8b1aa070
Almas Hosain Rifat <almashossainrifat@gmail.com>
2025-08-27 18:50:43 +0600

    added responsive section and also interactive js part

--- index.js
+++ index.js
@@ -18,18 +18,62 @@ for(let i = 0; i < heartBtns.length; i++ ) {
 // Copy Icon Click Counter 
 
 
-const copyCounter = document.querySelector('#copy button');
+const copyCounter = document.querySelector('#copyCounter');
+
+let copyCount = parseInt(copyCounter.textContent)
 
 const copyBtns = document.querySelectorAll('.copy_btn');
 
-let copyCount = 0;
 
 for (let i = 0; i < copyBtns.length; i++ ) {
     copyBtns[i].addEventListener('click',function(){
-        const copies  = copyBtns[i].closest('.copy');
-        const call = copies.querySelector('.emergency').textContent
+        const copies  = copyBtns[i].closest('.helpline');
+        const call = copies.querySelector('.emgNum').textContent
     
+        navigator.clipboard.writeText(call)
+        alert(`${call} copied to clipboard!`);
 
 copyCount++
 copyCounter.textContent = copyCount })
     }
+
+// Call Section 
+
+let coins = 100;
+
+const allCallButton = document.querySelectorAll('.call_btn')
+const coinCounter = document.querySelector('#coin h1')
+
+for (let i = 0; i < allCallButton.length; i++) {
+    allCallButton[i].addEventListener('click', function() {
+         const card = allCallButton[i].closest('.helpline');
+
+         const phoneName = card.querySelector('h1').textContent
+         const phoneNum = card.querySelector('.emgNum').textContent
+
+        if(coins < 20){
+            alert("You don't have enough coins to make this call!");
+            return;
+        }
+
+        coins -= 20;
+        coinCounter.textContent = coins;
+
+        const time = new Date().toLocaleTimeString();
+
+        alert(`Calling ${phoneName} at ${phoneNum}`);
+    
+
+        // Add to history
+        const historyElement = document.createElement('div');
+        historyElement.classList.add('flex','justify-between','border-b','p-2');
+        historyElement.innerHTML = `<span>${phoneName}</span><span>${phoneNum} | ${time}</span>`;
+        call_list.appendChild(historyElement);
+        })
+}
+
+const clearBtn = document.querySelector('#delete_all_list');
+
+clearBtn.onclick = function() {
+    call_list.innerHTML = '';
+};
